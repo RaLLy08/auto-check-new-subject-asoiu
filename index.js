108b2a187ce12c72941b58084cae7fa23fd5a01f
azadk <azadk@forit.az>
2021-02-03 17:48:31 +0400

    finish

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const Browser = require('zombie');
 
-const LOGIN_URL = 'http://exam.asoiu.edu.az/studentLogin';
-const PROFILE_URL = 'http://exam.asoiu.edu.az/studentProfile'
-const CHECK_TIME = 60000;
-const CHOOSE_DELAY = 300000;
-let urlValue = undefined;
-//require('events').EventEmitter.defaultMaxListeners = Infinity;
-const browser1 = new Browser();
-const browser2 = new Browser();
+const LOGIN_URL = 'https://asoiuexam.com/studentLogin';
+const mail = 'azad.kichibayov.y@asoiu.edu.az'
+const password = 'yvntEsDZ';
 
+const browser = new Browser();
+
+const sleep = mSec => new Promise((res, rej) => setTimeout(() => res(), mSec))
 
 const login = (browser, log, pass) => new Promise((resolve, reject) => {
   browser.visit(LOGIN_URL, () => {
@@ -31,102 +29,44 @@ const login = (browser, log, pass) => new Promise((resolve, reject) => {
         }
   })
 })
- 
-const visit = (browser, url) => new Promise((resolve, reject) => {
-  browser.visit(url, () => {
-    browser.wait()
-      .then(function() {
-        //console.log(status, respUrl, url);
-        if (browser.response && (browser.response.status === 200)) resolve(browser.response.body);
-        else reject()
-      }, () => {
-        if ((browser.response.status === 200)) resolve(browser.response.body);
-        else reject()
-        consoleText('error in visit');
-      })
-  })
-})
+// 2000-6000
+// 4994 - 7000
+const init = async (browser, log, pass) => {
+  await login(browser, log, pass);
+  let cycle = 7000;
 
-const autoCheckSubject = (browser) => {
-  autoVisit(browser, PROFILE_URL)
-    .then((html) => {
-      const lastSubjectUrl = checkSubjects(html);
-  
-      if (lastSubjectUrl) {
-        consoleText('changed');
-        setTimeout(() => {
-          chooseNewSubject(lastSubjectUrl);
-        }, CHOOSE_DELAY);
-      } else {
-        consoleText('none');
-        setTimeout(() => {
-          autoCheckSubject(browser)
-        }, CHECK_TIME);
-      }
-    })
-} 
+  const selection = async () => {
+    await browser.visit(`http://asoiuexam.com/selectTicketAct/1136/809/13${cycle}/1`, () => console.log(cycle, 'VISITING'))
+    await browser.wait();
 
-const checkSubjects = (htmlBody) => {
-    console.log(new Date());
+    cycle += 1;
+    // await sleep(500)
 
-    const tableFrom = htmlBody.indexOf('<tbody>')
-    const tableEnd = htmlBody.indexOf('</tbody>')
-    const table = htmlBody.slice(tableFrom, tableEnd).replace(/ +/g, ' ').trim();
-    const urlArr = table.match(/(?:https?:\/\/)?(?:[\w\.]+)\.(?:[a-z]{2,6}\.?)(?:\/[\w\.]*)*\/?/g);
-    const lastSubjectUrl = urlArr[urlArr.length - 1];
-  
-    if ((urlArr.length === urlValue) || !urlValue) {
-      urlValue = urlArr.length;
-      console.log(urlArr);
-      //return lastSubjectUrl
-    } else {
-      return lastSubjectUrl
+    if (cycle % 500 === 0) {
+      console.log('checkpoint')
+      var isPageOpened = await new Promise((y, n) => browser.visit(`https://asoiuexam.com/showTicket/809`, () => y(!browser.redirected)))
+
+      await browser.wait();
     }
+    // const isPageOpened = await new Promise((y, n) => browser.visit(`https://asoiuexam.com/showTicket/809`, () => y(!browser.redirected)))
+    // await browser.wait();
+    
+    if (cycle >= 9999 || isPageOpened) {
+      consoleText(cycle);
+      return;
+    } else {
+      selection()
+    }
+  }
+
+  await selection()
 }
 
-const chooseNewSubject = (lastSubjectUrl) => {
-  loginFirst(lastSubjectUrl)
-    .then(() => {
-      autoVisit(browser2, lastSubjectUrl).then(() => consoleText('elnur.maharramov.e@asoiu.edu.az selected subject successfully'));
-      autoVisit(browser1, lastSubjectUrl).then(() => consoleText('azad.kichibayov.y@asoiu.edu.az selected subject successfully'));
-    })
-}
 
-const loginFirst = () => new Promise(resolve => {
-  autoLogin(browser2, 'elnur.maharramov.e@asoiu.edu.az', 'password')
-    .then(() => {
-      resolve();
-    });
-})
- 
-//logines while error doesn't pressent
-const autoLogin = (browser, log, pass, hasAutoCheck) => new Promise(resolve => {
-  login(browser, log, pass)
-    .then(() => {
-      if (hasAutoCheck) autoCheckSubject(browser);
-      resolve()
-      consoleText(`${log} successfully logged in`);
-    })
-    .catch((status) => {
-      consoleText(`error status', ${status}, 'rerunning...', ${log}`);
-      setTimeout(() => {
-        autoLogin(browser, log, pass, hasAutoCheck);
-      }, 4000);
-    })
-})
+init(browser, mail, password);
 
-const autoVisit = (browser, url) => new Promise(resolve => {
-  visit(browser, url)
-    .then((html) => resolve(html))
-    .catch(() => {
-      consoleText(`error status, auto visit visit', 'rerunning...', ${url}`);
-      setTimeout(() => {
-        autoVisit(browser, url);
-      }, 4000);
-    })
-})
 
-autoLogin(browser1, 'azad.kichibayov.y@asoiu.edu.az', 'password', true)  //first logines and starting monitoring of changes in subjects
+
 
 const consoleText = (text) => {
   console.log(`\n*-----------------------------------------------------------------*\n ${text}\n*-----------------------------------------------------------------*`);
